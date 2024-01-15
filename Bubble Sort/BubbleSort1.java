11caa722e22d1147e4804eb8c5f905274e67c274
WMGT Wanninayake <127291805+GayaniTharu@users.noreply.github.com>
2024-01-15 21:54:45 +0530

    Add files via upload

---
+++ Bubble Sort/BubbleSort1.java
@@ -0,0 +1,26 @@
+public class BubbleSort1 {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array: ");
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+    }
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
